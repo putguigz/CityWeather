c5f05abed31e46e85c15c59aa82b8bb4d26b32db
Guillaume P <gpetit@student.42.fr>
2023-02-05 10:34:27 +0100

    [+] directory creation added for logger

--- srcs/Logger.cpp
+++ srcs/Logger.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <chrono>
 #include <ctime>
+#include <sys/stat.h>
+#include <string.h>
 
 static std::string produceTimeStamp(void){
     using std::chrono::system_clock;
@@ -15,9 +17,25 @@ static std::string produceTimeStamp(void){
     return (formatedCurrentDate);
 }
 
-void    logError(std::string errorMessage){
-    std::fstream fileStream ("log.txt", std::fstream::out | std::fstream::app);
+bool directoryExists(std::string dir){
+    struct stat st;
 
+    memset(&st, 0, sizeof(st));
+    return (stat(dir.c_str(), &st) == -1);
+}
+
+int createDirectory(std::string dir){
+    return (mkdir(dir.c_str(), 0700));
+}
+
+
+void    logError(std::string errorMessage){
+    std::string directory("logs");
+
+    if (directoryExists(directory))
+        createDirectory(directory);
+
+    std::fstream fileStream ("logs/log.txt", std::fstream::out | std::fstream::app);
     fileStream << "[" << produceTimeStamp() << "] " << errorMessage << std::endl;
     fileStream.close();
 }
