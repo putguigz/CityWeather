469afc9b7cd8353fdae1e5b2b6eeb2de1e3d3bcc
Guillaume P <gpetit@student.42.fr>
2023-02-07 10:01:10 +0100

    [fix] stable command line version

--- gtk_ui/gtk3to4converter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sed -i 's|            <property name="primary-icon-activatable">False</property>||g' cityWeather.gtk3.glade
-sed -i 's|            <property name="secondary-icon-activatable">False</property>||g' cityWeather.gtk3.glade
-sed -i 's|            <property name="primary-icon-sensitive">False</property>||g' cityWeather.gtk3.glade
-sed -i 's|            <property name="secondary-icon-sensitive">False</property>||g' cityWeather.gtk3.glade
-sed -i 's|            <property name="shadow-type">none</property>||g' cityWeather.gtk3.glade
-
-gtk4-builder-tool simplify --3to4 cityWeather.gtk3.glade > cityWeather.gtk4.glade
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -7,7 +7,6 @@
 #include "OpenMeteoApi.hpp"
 #include "FetchDataException.hpp"
 #include "Utils.hpp"
-#include "gtkmm.h"
 
 #define GEOCODE_URL     "https://geocoding-api.open-meteo.com/v1/search"
 #define OPENMETEO_URL   "https://api.open-meteo.com/v1/forecast"
@@ -16,85 +15,33 @@
 // for convenience
 using namespace std;
 
-namespace
-{
-Gtk::Window* pDialog = nullptr;
-Glib::RefPtr<Gtk::Application> app;
+//TODO DELETE WHEN APP GETS GRAPHICAL
+static string user_input(void){
+    string response;
 
-void on_text_typed(Gtk::SearchEntry *ptr)
-{
-  std::string city = ptr->get_text();
-
-  if (city.empty())
-    return;
-  GeoCodeApi citySearcher(GEOCODE_URL);
-
-
-  citySearcher.addSpecificParameters(city);
-  citySearcher.Get();
-  std::vector<City> cities = citySearcher.convertJsonResponseToMap();
-
-  for (auto city : cities)
-  {
-    std::cerr << city << endl;
-  }
+    cout << "Enter City Name:" << std::endl;
+    cin >> response;
+    return response;
 }
 
+int main(void){
+    string inputCity = user_input();
 
-void on_app_activate()
-{
-  // Load the GtkBuilder file and instantiate its widgets:
-  auto refBuilder = Gtk::Builder::create();
-  try
-  {
-    refBuilder->add_from_file("../gtk_ui/cityWeather.gtk4.glade");
-  }
-  catch(const Glib::FileError& ex)
-  {
-    std::cerr << "FileError: " << ex.what() << std::endl;
-    return;
-  }
-  catch(const Glib::MarkupError& ex)
-  {
-    std::cerr << "MarkupError: " << ex.what() << std::endl;
-    return;
-  }
-  catch(const Gtk::BuilderError& ex)
-  {
-    std::cerr << "BuilderError: " << ex.what() << std::endl;
-    return;
-  }
+    GeoCodeApi      request(GEOCODE_URL);
+    request.addSpecificParameters(inputCity);
+    request.Get();
 
-  // Get the GtkBuilder-instantiated dialog:
-  pDialog = refBuilder->get_widget<Gtk::Window>("MainWindow");
-  if (!pDialog)
-  {
-    std::cerr << "Could not get the dialog" << std::endl;
-    return;
-  }
+    auto resultCities = request.convertJsonResponseToMap();
+    for (citiesIterator it = resultCities.cbegin(); it!= resultCities.cend(); it++){
+        cout << *it << std::endl;
+    }
 
-  // Get the GtkBuilder-instantiated button, and connect a signal handler:
-  auto pSearchBar = refBuilder->get_widget<Gtk::SearchEntry>("SearchBar");
-  if (pSearchBar)
-  {
-    pSearchBar->signal_changed().connect([pSearchBar] () { on_text_typed(pSearchBar); });
-  }
-  // It's not possible to delete widgets after app->run() has returned.
-  // Delete the dialog with its child widgets before app->run() returns.
-  pDialog->signal_hide().connect([] () { delete pDialog; });
-
-
-  app->add_window(*pDialog);
-  pDialog->set_visible(true);
+    OpenMeteoApi    request2(OPENMETEO_URL);
+    request2.addSpecificParameters(resultCities[0]);
+    request2.Get();
+    
+    auto resultMeteos = request2.convertJsonResponseToMap();
+    for (auto it = resultMeteos.cbegin(); it != resultMeteos.cend(); it++){
+        cout << *it << std::endl;
+    }
 }
-} // anonymous namespace
-
-int main(int argc, char** argv)
-{
-  app = Gtk::Application::create("CityWeather");
-
-  app->signal_activate().connect([] () { on_app_activate(); });
-
-  return app->run(argc, argv);
-}
-
